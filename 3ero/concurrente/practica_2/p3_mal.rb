d271e4a698f6aaa10d135f0976e45016b6a9caf0
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2017-09-09 19:49:26 -0300

    Practicas de Concurrente

---
+++ 3ero/concurrente/practica_2/p3_mal.rb
@@ -0,0 +1,33 @@
+require 'concurrent'
+
+sem = Concurrent::Semaphore.new(6)
+alta = Concurrent::Semaphore.new(4)
+baja = Concurrent::Semaphore.new(5)
+l = 500
+k = 500
+threads = []
+
+l.times do
+  threads << Thread.new do
+    sleep
+    sem.acquire
+    alta.acquire
+    puts "alta usando BD #{sem.available_permits} "
+    sem.release
+    alta.release
+  end
+end
+
+k.times do
+  threads << Thread.new do
+    sleep
+    sem.acquire
+    baja.acquire
+    puts "baja usando BD #{sem.available_permits} "
+    sem.release
+    baja.release
+  end
+end
+
+threads.shuffle!
+threads.each(&:run)
