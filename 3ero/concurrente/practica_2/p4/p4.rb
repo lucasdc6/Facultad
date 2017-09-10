4326b10447acbf0833c830941b997cd64a919e20
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2017-09-09 22:51:38 -0300

    Se agrego codigo concurrente_like

---
+++ 3ero/concurrente/practica_2/p4/p4.rb
@@ -0,0 +1,43 @@
+require 'concurrent'
+
+ALUMNOS = 40
+
+tareas = ALUMNOS.times.collect { Concurrent::Semaphore.new(1) }
+mutex = ALUMNOS.times.collect { Concurrent::Semaphore.new(0) }
+correcciones = Array.new(ALUMNOS, false)
+entregas = []
+cant = ALUMNOS
+
+ALUMNOS.times do | i |
+  Thread.new do
+    while !correcciones[i]
+      tareas[i].acquire # Funcion P del 'lenguaje' de concurrente
+      sleep 1 # Se hace tarea
+      puts "entregar tarea alumno #{i} "
+      entregas.push i
+      tareas[i].release # Funcion V del 'lenguaje' de concurrente
+      mutex[i].acquire
+    end
+    puts "alumno #{i}: chau!"
+  end
+end
+
+
+maestra = Thread.new do
+  while cant != 0
+    if !entregas.empty?
+      entregas.pop.tap do |id|
+        tareas[id].acquire
+        [true, false].sample.tap do |correccion| # Se corrige la tarea al azar
+          correcciones[id] = correccion
+          cant -= 1 if correccion
+          puts "correccion tarea #{id} = #{correccion} - faltan #{cant} "
+          tareas[id].release
+          mutex[id].release
+        end
+      end
+    end
+  end
+end
+
+maestra.join # Se deja el hilo como el principal
