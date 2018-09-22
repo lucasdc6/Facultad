0bf8778e4e95bf22bbd129cddeb6893f6d93dd67
lucasdc <lucasdicunzolo@gmail.com>
2018-09-22 18:35:09 -0300

    update

--- 4to/2do_Semestre/PF/Practicas/Practica_1/punto-1.hs
+++ 4to/2do_Semestre/PF/Practicas/Practica_1/punto-1.hs
@@ -1,3 +1,4 @@
+-- Punto 1
 seven :: a -> Int
 seven x = 7
 
@@ -10,42 +11,106 @@ absolute :: Int -> Int
 absolute x = (sign x * x)
 
 absolute' :: Int -> Int
-absolute' x | x < 0 = x * -1
+absolute' x | x < 0 = x * (-1)
             | otherwise = x
 
-and' :: Bool -> Bool -> Bool
-and' x y | x == False = False
-         | y == False = False
-         | otherwise = True
+and' :: (Bool, Bool) -> Bool
+and' (x,y) | x == False = False
+           | y == False = False
+           | otherwise = True
 
-or' :: Bool -> Bool -> Bool
-or' x y | x == True = True
-        | y == True = True
-        | otherwise = False
+andC' :: Bool -> Bool -> Bool
+andC' x y | x == False = False
+          | y == False = False
+          | otherwise = True
+
+or' :: (Bool, Bool) -> Bool
+or' (x,y) | x == True = True
+          | y == True = True
+          | otherwise = False
+
+orC' :: Bool -> Bool -> Bool
+orC' x y | x == True = True
+         | y == True = True
+         | otherwise = False
 
 not' :: Bool -> Bool
 not' x | x == True = False
        | x == False = True
 
-xor' :: Bool -> Bool -> Bool
-xor' x y | x == y = False
-         | otherwise = True
+xor' :: (Bool, Bool) -> Bool
+xor' (x,y) | x == y = False
+           | otherwise = True
 
-dividesTo :: Int -> Int -> Bool
-dividesTo x y | (mod x y) == 0 = True
-              | otherwise = False
+xorC' :: Bool -> Bool -> Bool
+xorC' x y | x == y = False
+          | otherwise = True
 
-isMultiple :: Int -> Int -> Bool
-isMultiple x y = dividesTo x y
+dividesTo :: (Int, Int) -> Bool
+dividesTo (x,y) | (mod x y) == 0 = True
+                | otherwise = False
 
-isCommonDivisor :: Int -> (Int, Int) -> Bool
-isCommonDivisor n (x, y) | dividesTo x n == False = False
-                         | dividesTo y n == False = False
-                         | otherwise = True
+dividesToC :: Int -> Int -> Bool
+dividesToC x y | (mod x y) == 0 = True
+               | otherwise = False
+
+isMultiple :: (Int, Int) -> Bool
+isMultiple (x,y) = dividesTo(x,y)
+
+isMultipleC :: Int -> Int -> Bool
+isMultipleC x y = dividesToC x y
+
+isCommonDivisor :: (Int, (Int, Int)) -> Bool
+isCommonDivisor (n,(x, y)) | dividesTo(x,n) == False = False
+                           | dividesTo(y,n) == False = False
+                           | otherwise = True
+
+isCommonDivisorC :: Int -> (Int, Int) -> Bool
+isCommonDivisorC n (x, y) | dividesToC x n == False = False
+                          | dividesToC y n == False = False
+                          | otherwise = True
 
 swap :: (Int, Int) -> (Int, Int)
 swap (x, y) = (y, x)
 
-greaterThan :: Int -> Int -> Bool
-greaterThan x y | x > y = True
-                | otherwise = False
+swapC :: Int -> Int -> (Int,Int)
+swapC x y = (y, x)
+
+-- Punto 2
+
+greaterThan :: (Int, Int) -> Bool
+greaterThan (x,y)  | x > y = True
+                   | otherwise = False
+
+greaterThanC :: Int -> Int -> Bool
+greaterThanC x y | x > y = True
+                 | otherwise = False
+
+-- Punto 3
+
+sqr :: Int -> Int
+sqr x = x * x
+
+power4 :: Int -> Int
+power4 x =  let sqr y = y * y
+            in sqr (sqr x)
+
+power4' :: Int -> Int
+power4' x = sqr (sqr x)
+
+power4'' :: Int -> Int
+power4'' x = (sqr x) * (sqr x)
+
+-- Punto 4
+
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+-- Punto 9
+
+leapYear :: Int -> Bool
+leapYear x | dividesToC (mod x 100) 4 && mod x 100 > 0 = True
+           | dividesToC x 400 = True
+           | otherwise = False
---
+++ 4to/2do_Semestre/PF/Practicas/Practica_2/punto-2.hs
@@ -0,0 +1,97 @@
+-- Punto 4
+-- a
+-- first :: (a,b) -> a
+first (x,y) = x
+
+-- b
+-- second :: (a,b) -> b
+second (x, y) = y
+
+-- c
+-- const :: a -> b -> a
+const' x y = x
+
+-- d
+-- compose :: (a -> b) -> (c -> a) -> c -> b
+compose f g = (\x -> f (g x))
+
+-- e
+-- apply :: (a -> b) -> a -> b
+apply f x = f x
+
+-- f
+-- subst :: (a -> b -> c) -> (a -> b) -> a -> c
+subst f g x = f x (g x)
+
+-- g
+-- pairFunc: (a -> b, b -> a) -> b -> a -> (b -> a)
+pairFunc (f1,f2) x y = (f1 (f2 x), f2 (f1 y))
+
+
+-- Punto 6
+-- a) if (seven ’a’ < 1) then 5 else power4 2
+-- Correcta, el resultado es 4
+
+-- b) if False then True
+-- Incorrecto, el if, debe contar con then y else
+
+-- c) a := 4
+-- Incorrecto, No se asigna de esa manera
+
+-- d) (1 < x && x < 100) || x == True || x == False
+-- Incorrecto, se intenta comprar un entero (x), con un Bool (True y False)
+
+-- e) False == (1 < 3)
+-- Correcto, el resultado es False
+
+-- f) (1 < x < 2)
+-- Incorrecto, no se cuenta con un operador de ese tipo
+
+-- Punto 8
+-- a Colores
+data PrimaryColor = Red | Blue | Yellow deriving Show
+data SecondaryColor = Green | Violet | Orange deriving Show
+
+mix :: PrimaryColor -> PrimaryColor -> SecondaryColor
+mix Red Blue = Violet
+mix Red Yellow = Orange
+mix Blue Yellow = Green
+mix Blue Red = Violet
+mix Yellow Red = Orange
+mix Yellow Blue = Green
+
+-- b Puntos
+data Point = Point2D { x, y :: Float } | Point3D { x, y, z :: Float } deriving Show
+
+sqr :: Float -> Float
+sqr x = x * x
+
+xcoord :: Point -> Float
+xcoord (Point2D x _) = x
+xcoord (Point3D x _ _) = x
+
+ycoord :: Point -> Float
+ycoord (Point2D _ y) = y
+ycoord (Point3D _ y _) = y
+
+zcoord :: Point -> Float
+zcoord (Point3D _ _ z) = z
+
+pointModule :: Point -> Point -> Float
+pointModule (Point2D x1 y1) (Point2D x2 y2) = sqrt ((sqr (x2 - x1)) + (sqr (y2 - y1)))
+pointModule (Point3D x1 y1 z1) (Point3D x2 y2 z2) = sqrt ((sqr (x2 - x1)) + (sqr (y2 - y1)) + (sqr (z2 - z1)))
+pointModule (Point2D x1 y1) (Point3D x2 y2 z2) = sqrt ((sqr (x2 - x1)) + (sqr (y2 - y1)) + (sqr (z2)))
+pointModule (Point3D x1 y1 z1) (Point2D x2 y2) = sqrt ((sqr (x2 - x1)) + (sqr (y2 - y1)) + (sqr (z1)))
+
+pointAdd :: Point -> Point -> Point
+pointAdd (Point2D x1 y1) (Point2D x2 y2) = (Point2D (x1 + x2) (y1 + y2))
+pointAdd (Point3D x1 y1 z1) (Point3D x2 y2 z2) = (Point3D (x1 + x2) (y1 + y2) (z1 + z2))
+pointAdd (Point2D x1 y1) (Point3D x2 y2 z2) = (Point3D (x1 + x2) (y1 + y2) (z2))
+pointAdd (Point3D x1 y1 z1) (Point2D x2 y2) = (Point3D (x1 + x2) (y1 + y2) (z1))
+
+originDistance :: Point -> Point
+originDistance (Point2D x1 y1) = Point2D (abs(x1)) (abs(y1))
+originDistance (Point3D x1 y1 z1) = Point3D (abs(x1)) (abs(y1)) (abs(z1))
+
+-- c Figura
+data Shape = Circle Point Float | Rectangle Point Point
