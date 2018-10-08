34cd5fbc0faf7fe056a795df7b18684fd334ad9f
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-10-07 22:20:06 -0300

    update

--- 4to/2do_Semestre/PF/Practicas/Practica_5/practica_5.hs
+++ 4to/2do_Semestre/PF/Practicas/Practica_5/practica_5.hs
@@ -56,4 +56,40 @@ squares (x:xs) = x*x : squares xs
 
 lengths :: [[a]] -> [Int]
 lengths [] = []
-lengths (x:xs) = (length x) : lengths xs 
+lengths (x:xs) = (length x) : lengths xs
+
+-- order :: [(Int, Int)] -> [(Int, Int)]
+-- order x = ...
+-- order (x,xs) = ... x ... order xs ...
+
+order :: [(Int, Int)] -> [(Int, Int)]
+order [] = []
+order ((x1, x2):xs) = if x1*3 < x2 then (x1, x2) : order xs else order xs
+
+-- pairs :: [Int] -> [Int]
+-- pairs x = ...
+-- pairs (x:xs) = ... x ... pairs xs ...
+
+pairs :: [Int] -> [Int]
+pairs [] = []
+pairs (x:xs) = if even x then x : pairs xs else pairs xs
+
+-- chars :: [Char] -> [Char]
+-- chars x = ...
+-- chars (x:xs) = ... x ... chars xs ...
+
+chars :: [Char] -> [Char]
+chars [] = []
+chars (x:xs) = if isAlpha x then x : chars xs else chars xs
+
+-- moreThan :: [[a]] -> Int -> [[a]]
+-- moreThan x = ...
+-- moreThan (x:xs) = ... x ... moreThan xs ...
+
+moreThan :: [[a]] -> Int -> [[a]]
+moreThan [] _ = []
+moreThan (x:xs) num = if length x > num then x : moreThan xs num else moreThan xs num
+
+-- listOdd :: [[Int]] -> [Int]
+-- listOdd x = ...
+-- listOdd (x:xs) = ... x ... listOdd xs ...
