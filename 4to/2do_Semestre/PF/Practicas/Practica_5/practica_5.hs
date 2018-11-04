397a24540a7625c772ffdbca5dd7f848a6ad1539
lucasdc6 <lucasdicunzolo@gmail.com>
2018-11-04 09:32:41 -0300

    Updated file

--- 4to/2do_Semestre/PF/Practicas/Practica_5/practica_5.hs
+++ 4to/2do_Semestre/PF/Practicas/Practica_5/practica_5.hs
@@ -1,7 +1,7 @@
 import Data.Char
 
 -- listSum :: [a] -> Int
--- listSum x = ...
+-- listSum [] = ...
 -- listSum (x:xs) = ... x ... listSum xs ...
 
 listSum :: [a] -> Int
@@ -9,7 +9,7 @@ listSum [] = 0
 listSum (x:xs) = 1 + listSum xs
 
 -- listAny :: [Bool] -> Bool
--- listAny x = ...
+-- listAny [] = ...
 -- listAny (x:xs) = ... x ... listAny xs ...
 
 listAny :: [Bool] -> Bool
@@ -17,7 +17,7 @@ listAny [] = False
 listAny (x:xs) = x || listAny xs
 
 -- listAll :: [Bool] -> Bool
--- listAll x = ...
+-- listAll [] = ...
 -- listAll (x:xs) = ... x ... listAll xs ...
 
 listAll :: [Bool] -> Bool
@@ -26,7 +26,7 @@ listAll [x] = x
 listAll (x:xs) = x && listAll xs
 
 -- codes :: [Char] -> [Int]
--- codes x = ...
+-- codes [] = ...
 -- codes (x:xs) = ... x ... codes xs ...
 
 
@@ -35,7 +35,7 @@ codes [] = []
 codes (x:xs) = (ord x) : codes xs
 
 -- remainders :: [Int] -> Int -> Int
--- remainders x = ...
+-- remainders [] = ...
 -- remainders (x:xs) = ... x ... remainders xs ...
 
 remainders :: [Int] -> Int -> [Int]
@@ -43,7 +43,7 @@ remainders [] _ = []
 remainders (x:xs) num = (x `div` num) : remainders xs num
 
 -- squares :: [Int] -> [Int]
--- squares x = ...
+-- squares [] = ...
 -- squares (x:xs) = ... x ... squares xs ...
 
 squares :: [Int] -> [Int]
@@ -51,7 +51,7 @@ squares [] = []
 squares (x:xs) = x*x : squares xs
 
 -- lengths :: [[a]] -> Int
--- lengths x = ...
+-- lengths [] = ...
 -- lengths (x:xs) = ... x ... lengths xs ...
 
 lengths :: [[a]] -> [Int]
@@ -59,45 +59,56 @@ lengths [] = []
 lengths (x:xs) = (length x) : lengths xs
 
 -- order :: [(Int, Int)] -> [(Int, Int)]
--- order x = ...
+-- order [] = ...
 -- order (x,xs) = ... x ... order xs ...
 
 order :: [(Int, Int)] -> [(Int, Int)]
 order [] = []
-order ((x1, x2):xs) = if x1*3 < x2 then (x1, x2) : order xs else order xs
+order ((x1, x2):xs) = if x1*3 < x2
+                        then (x1, x2) : order xs
+                        else order xs
 
 -- pairs :: [Int] -> [Int]
--- pairs x = ...
+-- pairs [] = ...
 -- pairs (x:xs) = ... x ... pairs xs ...
 
 pairs :: [Int] -> [Int]
 pairs [] = []
-pairs (x:xs) = if even x then x : pairs xs else pairs xs
+pairs (x:xs) = if even x
+                    then x : pairs xs
+                    else pairs xs
 
 -- chars :: [Char] -> [Char]
--- chars x = ...
+-- chars [] = ...
 -- chars (x:xs) = ... x ... chars xs ...
 
 chars :: [Char] -> [Char]
 chars [] = []
-chars (x:xs) = if isAlpha x then x : chars xs else chars xs
+chars (x:xs) = if isAlpha x
+                    then x : chars xs
+                    else chars xs
 
 -- moreThan :: [[a]] -> Int -> [[a]]
--- moreThan x = ...
+-- moreThan [] = ...
 -- moreThan (x:xs) = ... x ... moreThan xs ...
 
 moreThan :: [[a]] -> Int -> [[a]]
 moreThan [] _ = []
-moreThan (x:xs) num = if length x > num then x : moreThan xs num else moreThan xs num
+moreThan (x:xs) num = if length x > num
+                        then x : moreThan xs num
+                        else moreThan xs num
 
 -- listOdd :: [[Int]] -> [Int]
--- listOdd x = ...
+-- listOdd [] = ...
 -- listOdd (x:xs) = ... x ... listOdd xs ...
 
 listOdd :: [[Int]] -> [Int]
 listOdd [] = []
 listOdd (x:xs) = odds x ++ listOdd xs
 
+-- odds :: [Int] -> [Int]
+-- odds [] = ...
+-- odds (x:xs) = ... x ... odds xs ...
 odds :: [Int] -> [Int]
 odds [] = []
 odds (x:xs) = if odd x
