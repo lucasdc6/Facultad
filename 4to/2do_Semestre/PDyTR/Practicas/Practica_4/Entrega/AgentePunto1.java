2aaf8a9e21475b0ccf407a27e0eba599207803ca
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2018-11-06 17:04:36 -0300

    UPDATE

--- 4to/2do_Semestre/PDyTR/Practicas/Practica_4/Entrega/AgentePunto1.java
+++ 4to/2do_Semestre/PDyTR/Practicas/Practica_4/Entrega/AgentePunto1.java
@@ -1,11 +1,17 @@
 import jade.core.*;
+import jade.util.leap.Serializable;
+import jade.wrapper.ContainerController;
 import java.util.ArrayList;
-public class AgentePunto1	 extends Agent
+import java.util.Iterator;
+
+public class AgentePunto1 extends Agent
 {
 
 	private Location origen;
 	private long startTime;
-	private ArrayList<String> containers;
+	private ArrayList<String> containers  = new ArrayList<String>();
+	private int index;
+	private ArrayList<ContainerInfo> info = new ArrayList<ContainerInfo>();
 	// Ejecutado por unica vez en la creacion
 	public void setup()
 	{
@@ -13,19 +19,26 @@ public class AgentePunto1	 extends Agent
 		System.out.println("\n\nHola, agente con nombre local " + getLocalName());
 		System.out.println("Y nombre completo... " + getName());
 		System.out.println("Y en location " + this.origen.getID() + "\n\n");
-		// Para migrar el agente
-
+		
+		//Creacion de los containers
 		for (int i=0; i<10;i++){
 			String containerName="Contenedor-"+i;
-			createContainer(containerName);
-			containers.add(containerName);
+			if (!containerName.equals(origen.getName())) {
+				createContainer(containerName);
+				containers.add(containerName);
+			}
 		}
+		//agrego el origen para que sea el final de la cadena
 		containers.add(this.origen.getName());
+		
 		try {
-			ContainerID destino = new ContainerID(containers.next(), null);
+			index=0;
+			// Para migrar el agente
+			ContainerID destino = new ContainerID(containers.get(index++), null);
 			System.out.println("Migrando el agente a " + destino.getID());
 			startTime = System.currentTimeMillis();    
 			doMove(destino);
+			System.out.println("WaAASSAAAA");
 		} catch (Exception e) {
 			System.out.println("\n\n\nNo fue posible migrar el agente\n\n\n");}
 	}
@@ -35,22 +48,43 @@ public class AgentePunto1	 extends Agent
 	{
 		Location actual = here();
 		if (actual.getName().equals(origen.getName())){
-			long finishTime=System.currentTimeMillis() - startTime;
-			System.out.println("Termine la vuelta, tiempo: "+ finishTime +" milisegundos");
+			this.originAction();
 		}
 		else{
-			System.out.println("\n\nHola, agente migrado con nombre local " + getLocalName());
-			System.out.println("Y nombre completo... " + getName());
-			System.out.println("Free memory (bytes): " + 
-  			java.lang.Runtime.getRuntime().freeMemory());
-			System.out.println("Y en location " + actual.getID() + "\n\n");
-			try {
-				ContainerID destino = new ContainerID(origen.getName(), null);
-				System.out.println("Migrando el agente a " + destino.getID());
-				doMove(destino);
-			} catch (Exception e) {
-				System.out.println("\n\n\nNo fue posible migrar el agente\n\n\n");
-			}
+			this.remoteAction(actual);
+		}
+	}
+
+	private void remoteAction(Location actual) {
+		long startContainerTime =System.currentTimeMillis();    
+		ContainerInfo currentContainerInfo= new ContainerInfo();
+		System.out.println("\n\nHola, agente migrado con nombre local " + getLocalName());
+		System.out.println("Y nombre completo... " + getName());
+		currentContainerInfo.setFreeMemory(java.lang.Runtime.getRuntime().freeMemory());
+		currentContainerInfo.setName(actual.getName());
+		try {
+			ContainerID destino = new ContainerID(containers.get(index++), null);
+			System.out.println("Migrando el agente a " + destino.getID());
+			info.add(currentContainerInfo);
+			long finishContainerTime=System.currentTimeMillis() - startContainerTime;
+			currentContainerInfo.setProcessingTime(finishContainerTime);
+			doMove(destino);
+		} catch (Exception e) {
+			System.out.println("\n\n\nNo fue posible migrar el agente\n\n\n");
+		}		
+	}
+
+	private void originAction() {
+		long finishTime=System.currentTimeMillis() - startTime;
+		System.out.println("\n\n-----------------------------------------------------------");
+		System.out.println("Termine la vuelta, tiempo total: "+ finishTime +" milisegundos");
+		
+		for (ContainerInfo containerInfo : info) {
+			System.out.println("\n\nInformación container " + containerInfo.getName() +" :");
+			System.out.println("    Memoria libre: "+ (containerInfo.getFreeMemory()/1024)/1024 +"Mb");
+			System.out.println("    Tiempo de procesamiento: "+ containerInfo.getProcessingTime() +" milisegundos");
+			float processingPercentage = (containerInfo.getProcessingTime()*100)/finishTime;
+			System.out.println("    Carga de procesamiento: "+ processingPercentage +"%");
 		}
 	}
 
@@ -63,5 +97,31 @@ public class AgentePunto1	 extends Agent
 		profile.setParameter(Profile.MAIN_HOST, "localhost");
 		//create a non-main agent container
 		return runtime.createAgentContainer(profile);
+	}
+	
+	public class ContainerInfo implements Serializable {
+		
+		private long freeMemory;
+		private String name;
+		private long processingTime;
+		
+		public long getFreeMemory() {
+			return freeMemory;
+		}
+		public void setFreeMemory(long freeMemory) {
+			this.freeMemory = freeMemory;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public long getProcessingTime() {
+			return processingTime;
+		}
+		public void setProcessingTime(long processingTime) {
+			this.processingTime = processingTime;
+		}
 	}
 }
