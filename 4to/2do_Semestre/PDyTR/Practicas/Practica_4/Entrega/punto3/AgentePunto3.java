d15e38a419e86a3f8c97039679b788fd77a5ee8c
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2018-11-08 01:05:03 -0300

    UPDATE programacion distribuida, practica 4, ejercicio 3

--- 4to/2do_Semestre/PDyTR/Practicas/Practica_4/Entrega/punto3/AgentePunto3.java
+++ 4to/2do_Semestre/PDyTR/Practicas/Practica_4/Entrega/punto3/AgentePunto3.java
@@ -16,34 +16,89 @@ public class AgentePunto3 extends Agent
     private String sourcePath;
     private String destinationPath;
     private byte[] file;
+    private Location origen;
 
     public void setup()
     {
-        Location origen = here();
+        //Chequeo parametros y seteo variables
+        Object[] args = getArguments();
+        this.checkAndSetArguments(args);
+
+        //informacion del agente
+        this.origen = here();
         System.out.println("\n\nHola, agente con nombre local " + getLocalName());
         System.out.println("Y nombre completo... " + getName());
         System.out.println("Y en location " + origen.getID() + "\n\n");
+
     try {
+        //migracion a otro container
         ContainerID destino = new ContainerID("Main-Container", null);
         System.out.println("Migrando el agente a " + destino.getID());
-        this.action="write";
-        this.sourcePath="/tmp/file";
-        this.destinationPath="/tmp/file2";
-        this.file= Files.readAllBytes(Paths.get(this.sourcePath));
         doMove(destino);
     } catch (Exception e) {
-        System.out.println("\n\n\nNo fue posible migrar el agente\n\n\n");}
+        System.out.println("\n\n\nNo fue posible migrar el agente\n\n\n: " + e);}
     }
 
     protected void afterMove()
     {
-        Location origen = here();
+        //Muestro info del container actual
+        Location actual = here();
         System.out.println("\n\nHola, agente migrado con nombre local " + getLocalName());
         System.out.println("Y nombre completo... " + getName());
         System.out.println("Y en location " + origen.getID() + "\n\n");
-        this.write(this.destinationPath,this.file);
-        byte[] file=this.read(this.destinationPath,0);
-        this.write(this.sourcePath+"3",file);
+        try{
+        switch (this.action) {
+                    case "write":    
+                            System.out.println(this.destinationPath);
+                            this.write(this.destinationPath,this.file);
+                            System.out.println("\n\n-------------------------------------------------------------------------------------------------");
+                            System.out.println("El archivo "+this.sourcePath+" se escribió correctamente en el directorio remoto "+this.sourcePath);
+                            break;
+                    case "read":
+                            if(!actual.getName().equals(this.origen.getName())){
+                                this.file = this.read(this.destinationPath,0);
+                                doMove(new ContainerID(this.origen.getName(), null));
+                            }
+                            else{
+                                try{
+                                    Files.write(Paths.get(this.sourcePath), this.file,StandardOpenOption.APPEND);
+                                }
+                                catch (IOException e) {
+                                    Files.createFile(Paths.get(this.sourcePath));
+                                    Files.write(Paths.get(this.sourcePath), this.file,StandardOpenOption.APPEND);
+                                }
+                                System.out.println("\n\n-------------------------------------------------------------------------------------------------");
+                                System.out.println("El archivo "+this.destinationPath+" se leyó correctamente y se guardó en "+this.sourcePath);
+                            }
+                            break;
+                    case "rw":
+                    case "readwrite":
+                        if(!actual.getName().equals(this.origen.getName())){
+                                this.file = this.read(this.destinationPath,0);
+                                doMove(new ContainerID(this.origen.getName(), null));
+                            }
+                            else{
+                                try{
+                                    Files.write(Paths.get(this.sourcePath), this.file,StandardOpenOption.APPEND);
+                                }
+                                catch (IOException e) {
+                                    Files.createFile(Paths.get(this.sourcePath));
+                                    Files.write(Paths.get(this.sourcePath), this.file,StandardOpenOption.APPEND);
+                                }
+                                System.out.println("\n\n-------------------------------------------------------------------------------------------------");
+                                System.out.println("El archivo "+this.destinationPath+" se leyó correctamente y se guardó en "+this.sourcePath);
+                                this.action="write";
+                                this.destinationPath+="-copia";
+                                doMove(new ContainerID("Main-Container", null));
+                            }
+                            break;
+                    case "list":
+                    case "ls":
+                        this.list(this.destinationPath);
+                        break;
+            }
+        }catch (IOException e) {System.out.println(e);}
+        
     }
 
     private byte[] read(String path, int position)
@@ -53,7 +108,7 @@ public class AgentePunto3 extends Agent
             int fileSize=contents.length;
             contents=Arrays.copyOfRange(contents,position,fileSize);
             return contents;
-        } catch(Exception e) {
+        } catch(IOException e) {
             System.out.println(e);
             return new byte[0];
         }
@@ -71,13 +126,13 @@ public class AgentePunto3 extends Agent
             }
             System.out.println(data.length);
             return data.length;            
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println(e.toString());
             return -1;
         }
     }
 
-    private String list(String path)
+    private void list(String path)
     {
         try (DirectoryStream<Path> paths = Files.newDirectoryStream(Paths.get(path))) {
             String directoryPaths = "";
@@ -86,10 +141,52 @@ public class AgentePunto3 extends Agent
                 directoryPaths += p.toString();
                 directoryPaths += "\n"; 
             }
-            return directoryPaths;
+            System.out.println(directoryPaths);
         } catch(Exception e) {
             System.out.println(e);
-            return e.toString();
         }
+    }
+
+    private void checkAndSetArguments(Object[] args){
+        try{
+            switch ((String) args[0]) {
+                    case "write":    
+                    case "read":
+                    case "rw":
+                    case "readwrite":
+                            if (args.length != 3)
+                            {
+                                System.out.println("3 argument needed: command , local directory and remote directory");
+                                System.exit(1);
+                            }
+                            else {
+                                this.action          = (String) args[0];
+                                this.sourcePath      = (String) args[1];
+                                this.destinationPath = (String) args[2];
+                                if (((String) args[0]).equals("write"))
+                                  this.file          = Files.readAllBytes(Paths.get(this.sourcePath));
+                            }
+                            break;
+                    case "list":
+                    case "ls":
+                            if (args.length != 2)
+                            {
+                                System.out.println("2 argument needed: command and directory");
+                                System.exit(1);
+                            }
+                            else {
+                                this.action          = (String) args[0];
+                                this.destinationPath = (String) args[1];
+                            }
+                            break;
+                    
+                    default: System.out.println("Command unavailable");
+                            System.exit(1);
+                            break;
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }                           
     }
 }
