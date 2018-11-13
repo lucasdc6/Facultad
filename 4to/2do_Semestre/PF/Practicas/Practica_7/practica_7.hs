3e0032388064e3aa6f6ad2d48539d44e73158840
lucasdc6 <lucasdicunzolo@gmail.com>
2018-11-13 14:04:22 -0300

    Update files

--- 4to/2do_Semestre/PF/Practicas/Practica_7/practica_7.hs
+++ 4to/2do_Semestre/PF/Practicas/Practica_7/practica_7.hs
@@ -1,14 +1,27 @@
 import Data.Char
 import Data.List
 
+-- foldr :: (a -> b -> b) -> b -> [a] -> b
+--recr :: b -> (a -> [a] -> b -> b) -> [a] -> b
+recr z f xs = foldr (\x h (_:ys) -> f x ys (h ys)) (const z) xs xs
+
+
+-- recr' z f [] = ...
+-- recr' z f (x:xs) = x ... recr' xs ...
+recr' :: b -> (a -> [a] -> b -> b) -> [a] -> b
+recr' z f [] = z
+recr' z f (x:xs) = f x xs (recr' z f xs) 
+
+tail' = recr (error "Lista vacía") (\_ xs _ -> xs)
+
 sum' :: [Int] -> Int
 sum' = foldr (+) 0
 
 any' :: [Bool] -> Bool
-any' = foldr (\x y -> x || y) False
+any' = foldr (||) False
 
 all' :: [Bool] -> Bool
-all' = foldr (\x y -> x && y) True
+all' = foldr (&&) True
 
 codes' :: [Char] -> [Int]
 codes' = foldr ((:) . ord) []
@@ -19,6 +32,9 @@ remainders' = flip (\r -> foldr (\x xs -> (x `div` r) : xs) [])
 squares' :: [Int] -> [Int]
 squares' = foldr (\x xs -> (x * x) : xs) []
 
+squares'' :: [Int] -> [Int]
+squares'' = map (\x -> x*x)
+
 lengths' :: [[a]] -> [Int]
 lengths' = foldr ((:) . length) []
 
@@ -28,17 +44,24 @@ pairs' = foldr (\x xs -> if even x then x : xs else xs) []
 chars' :: [Char] -> [Char]
 chars' = foldr (\x xs -> if isAlpha x then x : xs else xs) []
 
--- moreThan' :: [[a]] -> Int -> [[a]]
+moreThan' :: [[a]] -> Int -> [[a]]
 moreThan' = flip (\n -> foldr (\xs xss -> if length xs > n then xs : xss else xss) [])
-
 
 ----------------------------------------------------------------
 hs :: [[Char]] -> Int
 hs = foldr (\str i -> if "he" `isPrefixOf` str then i+1 else i+0) 0
 
+hs' :: [[Char]] -> Int
+hs' x = length (filter (isPrefixOf "he") x)
+
 avgLength :: [[Char]] -> Int
 avgLength = \xs -> let lengths = lengths' xs
                     in (sum lengths) `div` (length lengths)
 
-adjacents :: [Int] -> [(Int, Int)]
-adjacents = foldr (\x xs -> (x, head xs)) []
+avgLength' :: [[Char]] -> Int
+avgLength' xs = (sum lengths) `div` (length lengths)
+                where lengths = lengths' xs
+
+adjacents :: [a] -> [(a,a)]
+adjacents = recr' [] (\x xs ys -> [(x, x)])
+
