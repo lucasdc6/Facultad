d96116bbb723e95df69b5ecc10b5e94551f3b653
lucasdc6 <lucasdicunzolo@gmail.com>
2019-04-07 17:30:34 -0300

    Material de paralelos

---
+++ 5to/1er_Semestre/Sistemas_Paralelos/Practicas/Practica_2/test.c
@@ -0,0 +1,49 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  int start;
+  int end;
+  int id;
+} options;
+
+void * test(void *arg) {
+  options *param = (options *) arg;
+
+  printf("Hello from %d - with start: %d and end %d\n", param->id, param->start, param->end);
+  return 0;
+}
+
+int main(int argc, char* argv[])
+{
+  // Check for thread_number
+  if (argc < 2) {
+    printf("You must specify a number\n");
+    return 1;
+  }
+
+  // Set thread_number and initialize threads ids array
+  int thread_number = atoi(argv[1]);
+  options params[thread_number];
+
+  // Declare threads config
+  pthread_attr_t attr;
+  pthread_t threads[thread_number];
+  pthread_attr_init(&attr);
+
+  // Run threads
+  for (int i = 0; i < thread_number; i++) {
+    params[i].start = i > 0 ? (i-1) * 30 : i * 30;
+    params[i].end = i > 0 ? 30 : i * 30;
+    params[i].id = i;
+    pthread_create(&threads[i], &attr, test, &params[i]);
+  }
+
+  // Wait for all threads
+  for (int i = 0; i < thread_number; i++) {
+    pthread_join(threads[i], NULL);
+  }
+
+  return 0;
+}
