4013c96e02cd4ec8e0b1dc86ae9bc9a147738357
lucasdc6 <lucasdicunzolo@gmail.com>
2019-04-25 20:13:40 -0300

    Added tp3

--- 5to/1er_Semestre/Sistemas_Paralelos/Practicas/Practica_2/src/punto6-sec.c
+++ 5to/1er_Semestre/Sistemas_Paralelos/Practicas/Practica_2/src/punto6-sec.c
@@ -6,6 +6,10 @@
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
 #define ANSI_COLOR_RESET "\x1b[0m"
 
+#ifdef DEBUG
+int debug = 0;
+#endif
+
 double dwalltime()
 {
 	double sec;
@@ -14,6 +18,31 @@ double dwalltime()
 	gettimeofday(&tv,NULL);
 	sec = tv.tv_sec + tv.tv_usec/1000000.0;
 	return sec;
+}
+
+void merge (int *a, int n, int m) {
+  int i, j, k;
+  int *x = malloc(n * sizeof (int));
+  for (i = 0, j = m, k = 0; k < n; k++) {
+    x[k] = j == n      ? a[i++]
+          : i == m      ? a[j++]
+          : a[j] < a[i] ? a[j++]
+          :               a[i++];
+  }
+  for (i = 0; i < n; i++) {
+    a[i] = x[i];
+  }
+  free(x);
+}
+
+
+void merge_sort (int *a, int n) {
+    if (n < 2)
+        return;
+    int m = n / 2;
+    merge_sort(a, m);
+    merge_sort(a + m, n - m);
+    merge(a, n, m);
 }
 
 void printf_vector(int *vector, int vector_size)
@@ -28,6 +57,15 @@ void printf_vector(int *vector, int vector_size)
   printf(" ]\n");
 }
 
+int check(int *vector, int vector_size) {
+  for (int i = 0; i < vector_size; i++) {
+    if ((i+1) < vector_size && vector[i] > vector[i+1]) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
 int main(int argc, char *argv[])
 {
   if (argc < 2) {
@@ -36,7 +74,6 @@ int main(int argc, char *argv[])
   }
 
   #ifdef DEBUG
-  int debug = 0;
   if (getenv("DEBUG")) {
     debug = atoi(getenv("DEBUG"));
     printf(ANSI_COLOR_RED "Debug mode - Level %d\n" ANSI_COLOR_RESET, debug);
@@ -46,7 +83,6 @@ int main(int argc, char *argv[])
   #endif
 
   int initial_time, vector_size = atoi(argv[1]);
-  int *vector;
 
   #ifdef DEBUG
   if (debug > 0) {
@@ -55,7 +91,7 @@ int main(int argc, char *argv[])
   }
   #endif
 
-  vector = (int*) malloc(sizeof(int) * vector_size);
+  int* vector = (int*) malloc(sizeof(int) * vector_size);
 
   for (int i = 0; i < vector_size; i++) {
     vector[i] = rand()%10;
@@ -63,7 +99,7 @@ int main(int argc, char *argv[])
 
   #ifdef DEBUG
   if (debug > 1) {
-    printf("Vector values: \n");
+    printf("Initial Vector: \n");
     printf_vector(vector, vector_size);
     printf("\n\n");
   }
@@ -71,9 +107,25 @@ int main(int argc, char *argv[])
 
   initial_time = dwalltime();
 
-
+  merge_sort(vector, vector_size);
 
   printf("Time %g\n", dwalltime() - initial_time);
+
+  #ifdef DEBUG
+  if (debug > 1) {
+    printf("Sorted Vector: \n");
+    printf_vector(vector, vector_size);
+    printf("\n\n");
+  }
+  #endif
+
+  if (check(vector, vector_size)) {
+    printf("OK!\n");
+  } else {
+    printf("NOPE!\n");
+  }
+
+  free(vector);
 
   return 0;
 }
